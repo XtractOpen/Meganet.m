9493bf02f83f45cfda93b15ae8ead04e7f7bd112
Lars Ruthotto <lruthotto@emory.edu>
2018-02-28 18:30:11 -0500

    made setupSTL deterministic if all examples are requested

--- examples/setupSTL.m
+++ examples/setupSTL.m
@@ -6,7 +6,11 @@ function[Ytrain,Ctrain,Yval,Cval ] = setupSTL(nTrain,nVal)
 
 load train.mat
 Ytrain = double(X);
-ptrain = randi(size(Ytrain,1),nTrain,1);
+if nTrain<size(Ytrain,1)
+    ptrain = randi(size(Ytrain,1),nTrain,1);
+else
+    ptrain = 1:size(Ytrain,1);
+end
 Ytrain = Ytrain(ptrain,:);
 nex = size(Ytrain,1);
 Ytrain = reshape(normalizeData(reshape(Ytrain',96*96,[])')',[],nex);
@@ -16,7 +20,11 @@ Ctrain(ind) = 1;
 
 load  test.mat
 Yval = double(X);
-pval = randi(size(Yval,1),nVal,1);
+if nVal<size(Yval,1)
+    pval = randi(size(Yval,1),nVal,1);
+else
+    pval = 1:size(Yval,1);
+end
 Yval = Yval(pval,:);
 nv = size(Yval,1);
 Yval = reshape(normalizeData(reshape(Yval',96*96,[])')',[],nv);
@@ -24,6 +32,5 @@ Cval = zeros(nv,10);
 ind    = sub2ind(size(Cval),(1:size(Cval,1))',y(pval));
 Cval(ind) = 1;
 
-Ytrain = Ytrain';
 Ctrain = Ctrain';
 Cval   = Cval';
