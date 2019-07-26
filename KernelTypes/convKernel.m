54266c6ab271600831288a746e76ff4d56188882
Lars Ruthotto <lruthotto@emory.edu>
2019-07-26 11:53:48 -0400

    convKernel: fixed output size when using different paddings

--- KernelTypes/convKernel.m
+++ KernelTypes/convKernel.m
@@ -68,7 +68,7 @@ classdef convKernel  < handle
         end
         
         function n = nImgOut(this)
-            n = [this.nImg(1:2)./this.stride this.sK(4)];
+            n = [(this.nImg(1:2) + this.pad - (this.sK(1:2)-1))./this.stride this.sK(4)];
         end
         function this = gpuVar(this,useGPU,precision)
         end
